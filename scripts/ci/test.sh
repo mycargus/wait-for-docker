32a6567f399e13b68121672d3f4b6f8fd907abe5
Michael Hargiss <mdhargiss@gmail.com>
2020-08-02 00:16:31 -0400

    improve ci

---
+++ scripts/ci/test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ev
+
+docker-compose up -d web
+
+docker-compose run --rm wait-for web:8080 && echo $?
+
+exit_code=$?
+echo "wait-for container exited with: ${exit_code}"
+
+if [[ "${exit_code}" == "0" ]]; then
+  echo ":: It's working!"
+else
+  echo ":: Test Failed :("
+  exit_code=1
+fi
+
+exit ${exit_code}
